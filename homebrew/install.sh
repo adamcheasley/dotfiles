f92ebe35c574c8ef7013275df48bc0ab48b7431e
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2016-01-29 11:51:59 +0000

    Update install.sh

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,6 +13,12 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark
+brew install grc coreutils spark wget
+
+echo "Getting total terminal dmg..."
+wget http://downloads.binaryage.com/TotalTerminal-1.4.11.dmg
+
+echo "Getting seqelpro..."
+wget https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg
 
 exit 0
